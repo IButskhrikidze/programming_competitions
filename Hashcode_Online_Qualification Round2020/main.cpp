9cc135549a8cf40a29cb1369a9739941c6c911ab
ibutskhrikidze <butskh@gmail.com>
2020-02-20 22:49:24 +0400

    lets goo

--- Hashcode_Online_Qualification Round2020/main.cpp
+++ Hashcode_Online_Qualification Round2020/main.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#defin ll long long
+#define ll long long
 
 using namespace std;
 
@@ -32,10 +32,16 @@ public:
 
 vector<Book>books;
 vector<Library>libraries;
+vector<Library>ans;
+
+vector<Library> solve(){
+    random_shuffle(libraries.begin(), libraries.end());
+    
+}
 
 int main(int argv, char **args){
-    string input_file = argv[1];
-    string output_file = argv[2];
+    string input_file = args[1];
+    string output_file = args[2];
 
     ifstream fin(input_file);
     ofstream fout(output_file);
@@ -53,6 +59,7 @@ int main(int argv, char **args){
 
     for(int i=0;i<L;i++){
         fin >> libraries[i].num_book >> libraries[i].signup >> libraries[i].scan_per_day;
+        libraries[i].ID = i;
         for(int j=0;j<libraries[i].num_book;j++){
             int id;
             fin >> id;
@@ -61,6 +68,15 @@ int main(int argv, char **args){
         sort(libraries[i].books.begin(), libraries[i].books.end());
     }
 
+    fout << ans.size() << endl;
+
+    for(auto library : ans){
+        fout << library.ID << ' ' << library.books.size() << endl;
+        for(auto book : library.books){
+            fout << book.ID << ' ';
+        }
+        fout << endl;
+    }
 
 
     return 0;
